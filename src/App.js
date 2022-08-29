40199e6f4937b9a2be482904249d204c4373da84
Gbemisola Odukoya <folaryz@gmail.com>
2022-08-29 22:52:22 +0100

    Started Ternary practise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  /**
+   * Challenge: Replace the if/else below with a ternary
+   * to determine the text that should display on the page
+   */
+  const isGoingOut = true;
+
+  let answer; // Use ternary here
+  if (isGoingOut === true) {
+    answer = "Yes";
+  } else {
+    answer = "No";
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="state">
+      <h1 className="state--title">Do I feel like going out tonight?</h1>
+      <div className="state--value">
+        <h1>{answer}</h1>
+      </div>
     </div>
   );
 }
-
-export default App;
