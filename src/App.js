043dde9d2e814bcda87786fb6a39e9a3d50d4bc0
Gbemisola Odukoya <folaryz@gmail.com>
2022-08-30 14:44:07 +0100

    More Ternary practice

--- src/App.js
+++ src/App.js
@@ -2,24 +2,32 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
+  const [isGoingOut, setIsGoingOut] = React.useState(true);
   /**
    * Challenge: Replace the if/else below with a ternary
    * to determine the text that should display on the page
    */
-  const isGoingOut = true;
+  // const isGoingOut = true;
 
-  let answer; // Use ternary here
-  if (isGoingOut === true) {
-    answer = "Yes";
-  } else {
-    answer = "No";
+  // uusing ternary
+  // let answer = isGoingOut ? "Yes" : "No";
+
+  // let answer; // Use ternary here
+  // if (isGoingOut === true) {
+  //   answer = "Yes";
+  // } else {
+  //   answer = "No";
+  // }
+
+  function changeMind() {
+    setIsGoingOut((prevState) => !prevState);
   }
 
   return (
     <div className="state">
       <h1 className="state--title">Do I feel like going out tonight?</h1>
-      <div className="state--value">
-        <h1>{answer}</h1>
+      <div onClick={changeMind} className="state--value">
+        <h1>{isGoingOut ? "Yes" : "No"}</h1>
       </div>
     </div>
   );
